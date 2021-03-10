0b94fa2efbf24181cb080c2dcd67f682cfb0b25c
Lenin Girón <lenin@dev.pagaloop.com>
2021-03-10 10:53:48 -0600

    first DTOs are added

---
+++ Base.Lgm.Core/Models/Dto/Request/UserRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Base.Lgm.Core.Models.Dto.Request
+{
+    public class UserRequest
+    {
+    }
+}
---
+++ Base.Lgm.Core/Models/Dto/Response/UserResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Base.Lgm.Core.Models.Dto.Response
+{
+    public class UserResponse
+    {
+    }
+}
